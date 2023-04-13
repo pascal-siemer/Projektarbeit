6c50567aa89bd25b0a4f8dfecd5f237cb8cfead2
pascal-siemer <95932011+pascal-siemer@users.noreply.github.com>
2023-04-13 09:04:48 +0200

    Update RoundHandler.py
    Comments

--- Endpoints/RoundHandler.py
+++ Endpoints/RoundHandler.py
@@ -7,5 +7,9 @@ class RoundHandler(IEndpoint):
     def __init__(self, game: Game):
         self.game = game
 
+    #set game.round_started
     async def handle_request(self, message: str) -> object:
         pass
+
+    #while game.round_status: send round stuff
+    #async def send
