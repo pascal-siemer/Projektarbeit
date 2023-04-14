c178046f281f0bd755de6a5dcf212fd4e7f98981
pascal-siemer <95932011+pascal-siemer@users.noreply.github.com>
2023-04-14 10:16:10 +0200

    Update RegistrationHandler.py

--- Endpoints/RegistrationHandler.py
+++ Endpoints/RegistrationHandler.py
@@ -1,15 +1,15 @@
 class RegistrationHandler:
     
-    def __init__(self, game: Game)
+    def __init__(self, game: Game):
         self.game = game
         
-    def handle_registration(self, message: str):
+    def handle_registration(self, message: Message) -> None:
         __add_new_player(message)
         
-    def __add_new_player(message: str)
+    def __add_new_player(message: Message) -> None:
         players = self.game.players
         for player in players:
             if player.name == message:
                 return
-        #hier fehlt der entsprechende Socket
-        
+        new_player = Player(message.value, message.websocket)
+        game.players.add(new_player)
