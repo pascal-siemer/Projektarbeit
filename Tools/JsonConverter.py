1ba7df3ad54001fe9b9181b78e448d50600c996a
pascal-siemer <95932011+pascal-siemer@users.noreply.github.com>
2023-04-14 09:14:35 +0200

    Update JsonConverter.py
    WIP definition

--- Tools/JsonConverter.py
+++ Tools/JsonConverter.py
@@ -6,3 +6,7 @@ class JsonConverter:
     @staticmethod
     def deserialize(deserializable: object) -> str:
         return json.dumps(deserializable, default=lambda obj: obj.__dict__)
+    
+    @staticmethod
+    def serialize(message: str, websocket) -> Message:
+        #message umbauen, neues Tool erstellen, welches die Teile aus Message seperiert.
