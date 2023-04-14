057af8a8a4ea36d81748aaf658a915fddfd9e068
pascal-siemer <95932011+pascal-siemer@users.noreply.github.com>
2023-04-14 10:09:28 +0200

    Create MessageProcessor.py
    WIP MessageProcessor that takes a string and returns a message object

---
+++ Tools/MessageProcessor.py
@@ -0,0 +1,25 @@
+class MessageProcessor:
+    
+    identifier_left = "<<"
+    identifier_right = ">>"
+    
+    @staticmethod
+    def create_message_object(message: str, websocket) -> Message:
+        return Message(message_type=MessageProcessor.get_message_type(message),
+                       websocket=websocket,
+                       value=get_message_data(message))
+    
+    @staticmethod
+    def get_message_type(message: str) -> str | None:
+        start_index = message.find(MessageProcessor.identifier_left) + len(MessageProcessor.identifier_left)
+        end_index = message.rfind(MessageProcessor.identifier_right)
+        if start_index == -1 or end_index == 1:
+            return None
+        return message[start_index:end_index]
+
+    @staticmethod
+    def get_message_data(message: str):
+        identifier = f"{MessageProcessor.identifier_left}" \
+                     f"{MessageProcessor.get_message_type(message)}" \
+                     f"{MessageProcessor.identifier_right}"
+        return message.replace(identifier, "")
